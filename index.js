4092011876fde330607ed30c84e21fc746a24f51
Sara Ghaedi <saraghaedi75@gmail.com>
2021-09-16 11:47:54 +0200

    Add JWT

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const userRouter = require("./routers/user");
 const imageRouter = require("./routers/image");
+const authRouter = require("./routers/auth");
 const PORT = process.env.PORT || 4000;
 const app = express();
 const jsonParser = express.json();
@@ -9,5 +10,6 @@ app.use(jsonParser);
 
 app.use("/users", userRouter);
 app.use("/images", imageRouter);
+app.use("/auth", authRouter);
 
 app.listen(PORT, () => console.log("Listening from port 4000"));
---
+++ routers/auth.js
@@ -0,0 +1,24 @@
+const { Router } = require("express");
+const { toJWT, toData } = require("../auth/jwt");
+
+const router = new Router();
+
+router.post("/login", async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      res.status(400).send({
+        message: "Please supply a valid email and password",
+      });
+    } else {
+      res.send({
+        jwt: toJWT({ userId: 1 }),
+      });
+    }
+  } catch (e) {
+    console.log(e.message);
+    next(e);
+  }
+});
+
+module.exports = router;
